ed6670e4dad82164875a3f465c5258bac6d286f4
Patrick Fung <patrick.fung@ryerson.ca>
2020-04-02 14:25:28 -0400

    add todos

--- scripts/configure-summon.js
+++ scripts/configure-summon.js
@@ -2,17 +2,20 @@ const SummonAdmin = require('../lib/summon-admin.js');
 
 const config = require('../config.js');
 
+
 (async () => {
   const summon = new SummonAdmin({
     ssClientCenterUrl : 'https://clientcenter.serialssolutions.com/CC/Login/Default.aspx?ReturnUrl=/CC/Library/Default.aspx?LibraryCode=RRP',
     ssClientCenterEmail : config.summon.ssClientCenterEmail,
     ssClientCenterPassword : config.summon.ssClientCenterPassword,
   });
-
+  
   await summon.open();
   
   // TODO: The script should figure out what to set the external script URL to on it's own.
   await summon.setExternalScript('https://ryersonlibrary.nyc3.digitaloceanspaces.com/rylib-custom-summon/v0.0.x/js/rylib-custom-summon.js');
-
+  
+  // TODO: Add configuration scripts for the "Custom Panels" section
+  
   await summon.close();
 })();
