d961b8b9266d715a6961dc26055a52dc4c9986cb
Patrick Fung <patrick.fung@ryerson.ca>
2020-04-02 14:24:07 -0400

    set up npm run configure-summon script

---
+++ scripts/configure-summon.js
@@ -0,0 +1,18 @@
+const SummonAdmin = require('../lib/summon-admin.js');
+
+const config = require('../config.js');
+
+(async () => {
+  const summon = new SummonAdmin({
+    ssClientCenterUrl : 'https://clientcenter.serialssolutions.com/CC/Login/Default.aspx?ReturnUrl=/CC/Library/Default.aspx?LibraryCode=RRP',
+    ssClientCenterEmail : config.summon.ssClientCenterEmail,
+    ssClientCenterPassword : config.summon.ssClientCenterPassword,
+  });
+
+  await summon.open();
+  
+  // TODO: The script should figure out what to set the external script URL to on it's own.
+  await summon.setExternalScript('https://ryersonlibrary.nyc3.digitaloceanspaces.com/rylib-custom-summon/v0.0.x/js/rylib-custom-summon.js');
+
+  await summon.close();
+})();
